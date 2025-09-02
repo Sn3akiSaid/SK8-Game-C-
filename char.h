66362ba20a0f361e31a43ec108b795158c70c4fa
Sn3akiWSL <taragoshevaleksandar@gmail.com>
2025-09-02 19:53:29 +0100

    Installed sfml library and incorporated a basic window that remains open.

---
+++ char.h
@@ -0,0 +1,11 @@
+#ifndef char_h
+#define char_h
+
+#include<iostream>
+
+class Char
+{
+  
+};
+
+#endif
--- main.cpp
+++ main.cpp
@@ -1,7 +1,20 @@
 #include<iostream>
+#include<SFML/Graphics.hpp> // NEED TO INSTALL GLFW
 #include"char.h"
 
 int main()
 {
-  std::cout<<"Hello World"<<std::endl;
+  sf::Window window(
+    sf::VideoMode(640, 480),
+    "Hello");
+  while (window.isOpen())
+  {
+    sf::Event event;
+    while (window.pollEvent(event))
+      if (event.type ==
+          sf::Event::Closed)
+          window.close();
+
+  }
+  return 0;
 }
