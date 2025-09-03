121eacfaf99eb1413f4056fb87488da4b5e321ec
Sn3akiWSL <taragoshevaleksandar@gmail.com>
2025-09-03 15:22:36 +0100

    Created basic circle and key press movement

--- main.cpp
+++ main.cpp
@@ -1,20 +1,54 @@
 #include<iostream>
-#include<SFML/Graphics.hpp> // NEED TO INSTALL GLFW
+#include<SFML/Graphics.hpp> 
 #include"char.h"
 
 int main()
 {
-  sf::Window window(
-    sf::VideoMode(640, 480),
-    "Hello");
+  const int winWidth = 640;
+  const int winHeight = 480;  
+  sf::RenderWindow window(sf::VideoMode(winWidth, winHeight), "Hello");
+  // window.setFramerateLimit(60);      
+
+  sf::CircleShape circle(50);
+  circle.setFillColor(sf::Color::Magenta);
+  circle.setPosition(300.0f, 300.0f);
+  float circleMoveSpeed = 0.01f;
+
+  // sf::Font myFont;
+  // if (!myFont.loadFromFile("fonts/tech.ttf"))
+  // {
+  //   std::cerr << "Can't load font\n";
+  //   exit(-1);
+  // }
+
+  // Keep window open
   while (window.isOpen())
   {
+    // Event Handling
     sf::Event event;
     while (window.pollEvent(event))
-      if (event.type ==
-          sf::Event::Closed)
-          window.close();
+    {
+      // Trigger window closing when window is closed
+      if (event.type == sf::Event::Closed)
+      {
+        std::cout << "Closing window" << std::endl;
+        window.close();
+      }
+      // Key press event
+      if (event.type == sf::Event::KeyPressed)
+      {
+        std::cout << "Key pressed: " << event.key.code << std::endl;
+        if (event.key.code == sf::Keyboard::A)
+        {
+          circle.move({-5.f,0.f});
+        }
 
+      }
+    window.clear();
+    window.draw(circle);
+    window.display();
+    }
   }
+
   return 0;
 }
